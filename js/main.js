032c0c16490fad81926fb113fc27337563039b56
mjtheriot4 <mther16@lsu.edu>
2018-01-24 00:41:04 -0600

    re-fix button on about page

--- js/main.js
+++ js/main.js
@@ -6,17 +6,38 @@ var windowHeight;
 
 $(document).ready(function(){
   $('.image-link').magnificPopup({type:'image'});
-  $('.iframe-link').magnificPopup({type:'iframe'});
-  // $('.iframe-link').magnificPopup({
-  //   type: 'iframe',
-  //   iframe: {
-  //      markup: '<div class="mfp-iframe-scaler">'+
-  //              '<div class="mfp-close"></div>'+
-  //              '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-  //              '<div class="mfp-title">Some caption</div>'+
-  //              '</div>'
-  //   }
-  // });
+  // $('.iframe-link').magnificPopup({type:'iframe'});
+  $('.iframe-link').magnificPopup({type:
+    'iframe',
+    iframe: {
+    markup: '<div class="mfp-iframe-scaler">'+
+              '<div class="mfp-close"></div>'+
+              '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+            '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+    patterns: {
+      youtube: {
+        index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+        id: 'v=',
+        pid: 'v=', // String that splits URL in a two parts, second part should be %id%
+        // Or null - full URL will be returned
+        // Or a function that should return %id%, for example:
+        // id: function(url) { return 'parsed id'; }
+        src: '//www.youtube.com/embed/%id%?&loop=1&playlist=%pid%' // URL that will be set as a source for iframe.
+      },
+      vimeo: {
+        index: 'vimeo.com/',
+        id: '/',
+        src: '//player.vimeo.com/video/%id%?autoplay=1'
+      },
+      gmaps: {
+        index: '//maps.google.',
+        src: '%id%&output=embed'
+      }
+      // you may add here more sources
+    },
+      srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+    }
+  });
   galleries();
   // toggle side menu
   bug.click(function(){
