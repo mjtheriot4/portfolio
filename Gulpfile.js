31b203d620903a25ed997a93fd56d92a7877fc6d
mjtheriot4 <mther16@lsu.edu>
2018-02-15 16:23:23 -0600

    added file compression

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
 
 //task compiles the sass into css
 gulp.task('compileSass', function() {
@@ -7,8 +8,14 @@ gulp.task('compileSass', function() {
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('css/'));
 });
-
+//task compresses images
+gulp.task('compressImg', function() {
+  gulp.src('src-img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('img'));
+});
 //Watch task
 gulp.task('default',function() {
     gulp.watch('sass/**/*.scss',['compileSass']);
+    // gulp.watch('src-assets/*',['compressImg']);
 });
